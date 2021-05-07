43e55d99ab35ef55baae20355d2e366fd0758504
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 09:48:09 +0000

    GridSection.js: changed

--- src/components/GridSection.js
+++ src/components/GridSection.js
@@ -43,6 +43,7 @@ export default class GridSection extends React.Component {
             		</div>
             	</div>
             	)}
+                <div className=""
             	{_.get(section, 'actions', null) && (
             	<div className={classNames('container', 'container--medium', {'mt-4': grid_gap_y !== 'large', 'mt-3': grid_gap_y === 'large'})}>
             		<div className={classNames('section__actions', 'btn-group', {'justify-center': align_x === 'center', 'justify-end': align_x === 'right'})}>
